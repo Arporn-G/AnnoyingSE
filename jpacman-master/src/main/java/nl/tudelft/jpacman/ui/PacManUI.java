752fabdcc99bfb822673a86628cc49b0031d44a5
Arporn-G <90462932+Arporn-G@users.noreply.github.com>
2023-03-10 15:09:52 +0700

    setResizable

--- jpacman-master/src/main/java/nl/tudelft/jpacman/ui/PacManUI.java
+++ jpacman-master/src/main/java/nl/tudelft/jpacman/ui/PacManUI.java
@@ -80,7 +80,7 @@ public class PacManUI extends JFrame {
         this.boardPanel = new BoardPanel(game);
         this.keys = new PacKeyListener(keyMappings);
         addKeyListener(keys);
-
+        this.setResizable(false);
         this.buttonPanel = new ButtonPanel(buttons, this);
 
         this.scorePanel = new ScorePanel(game.getPlayers());
